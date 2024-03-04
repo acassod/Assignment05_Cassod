ecab199dc63c64196723233b62aa5a0909ed9131
Ashnu <cassod.a@northeastern.edu>
2024-03-04 16:53:17 -0500

    added addNumbers

--- Assignment05_Cassod.py
+++ Assignment05_Cassod.py
@@ -5,8 +5,13 @@ class BasicMathOperations:
         last = input("Enter your last name.")
         return "Hello, " + first + " " + last + "!"
     
+    def addNumbers(self):
+        num1 = float(input("Enter the first number."))
+        num2 = float(input("Enter the second number."))
+        return "Sum: " + str(num1 + num2)
+        
+        
 b = BasicMathOperations()
-print(b.greetUser())
-
+print(b.addNumbers())
 
     
